f3be1315f6b367c1a35b1372e0ec778150f6402d
Zipangri2683 <s.inage.asianzu@gmail.com>
2023-06-21 02:18:36 +0900

    ツイートの詳細を表示する

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
 <<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 =======
   root to: 'tweets#index'
   resources :tweets
 >>>>>>> Stashed changes
+=======
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+>>>>>>> ac715ac (first commit)
 end
