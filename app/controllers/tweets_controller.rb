4a027e12680b61af40eeda2b886bec55b373ef84
Zipangri2683 <s.inage.asianzu@gmail.com>
2023-06-26 19:30:05 +0900

    ツイートの詳細を表示する3

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,5 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet, only: [:edit, :show]
-
+  
   def index
     @tweets = Tweet.all
   end
@@ -19,6 +18,7 @@ class TweetsController < ApplicationController
   end
 
   def edit
+    @tweet = Tweet.find(params[:id])
   end
 
   def update
@@ -27,6 +27,7 @@ class TweetsController < ApplicationController
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   private
@@ -34,7 +35,4 @@ class TweetsController < ApplicationController
     params.require(:tweet).permit(:name, :image, :text)
   end
 
-  def set_tweet
-    @tweet = Tweet.find(params[:id])
-  end
 end
